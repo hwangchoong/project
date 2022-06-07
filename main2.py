9e2e4c3f7c9a1bbb35415bac4e28aa66e2dee5c9
HwangChonng <gkcnd@naver.com>
2022-06-07 17:34:15 +0900

    메세지입력

--- main2.py
+++ main2.py
@@ -1,6 +1,9 @@
-import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout
+import sys,socket
+from PyQt5.QtWidgets import *
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+svrIP = input(("Server IP(default:127.0.0.1"))
 class Myapp(QWidget):
 
     def __init__(self):
@@ -16,8 +19,25 @@ class Myapp(QWidget):
         btn1.setCheckable(True)
         btn1.toggle()
         btn1.resize(100,100)
-        btn2 = QPushButton('학생점수확인')
+        btn2 = QPushButton('학생점수확인버튼')
         btn2.setText('')
+        btn2.resize(100,100)
+        btn3 = QPushButton('문제출제버튼')
+        btn3.setText('')
+        btn3.resize(100,100)
+        btn4 = QPushButton('상담버튼')
+        btn4.setText('')
+        btn4.resize(100,100)
+        btn5 = QPushButton('확인할 학생버튼')
+        btn5.setText('')
+        btn5.resize(100,100)
+
+        vbox=QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+        vbox.addWidget(btn4)
+        vbox.addWidget(btn5)
 
 
 
